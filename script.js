b19007272e76433dc9971e98415e0df267faf839
Ryan Wong <ryanjameswong@outlook.com>
2024-02-12 18:33:32 -0500

    Include search function. Need error check

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-async function getWeatherData(city) {
-  const response = await
-  fetch(
-    `https://api.weatherapi.com/v1/current.json?key=52c4081c780640549c415817241102&q=${city}`,
-    { mode: 'cors' },
-  );
-  const weatherData = await response.json();
-  console.log(weatherData);
-  processWeatherData(weatherData);
-}
+const errorMessage = document.getElementById('error-message');
 
 const processWeatherData = (weatherData) => {
   const { feelslike_c: celcius, feelslike_f: farenheit, condition } = weatherData.current;
   console.log({ celcius, farenheit, condition });
 };
+
+async function getWeatherData(city) {
+  try {
+    const response = await
+    fetch(
+      `https://api.weatherapi.com/v1/current.json?key=52c4081c780640549c415817241102&q=${city}`,
+      { mode: 'cors' },
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    if (weatherData) {
+      processWeatherData(weatherData);
+    }
+  } catch (error) {
+    errorMessage.textContent = 'Could not find city';
+    alert(error);
+    console.log(error);
+  }
+}
 
 getWeatherData('toronto');
 
@@ -23,7 +33,6 @@ searchLocationButton.addEventListener('click', () => {
   const newCity = searchLocationInput.value;
   if (newCity) {
     getWeatherData(newCity);
-  } else {
-    return;
+    searchLocationInput.value = '';
   }
 });

dabf90ecd2b31edaaa70e8fd17fd9a625671e44d
Ryan Wong <ryanjameswong@outlook.com>
2024-02-14 14:04:40 -0500

    Add UI to display weather information

--- src/UIDisplay.js
+++ src/UIDisplay.js
@@ -1,15 +1,26 @@
 const display = document.getElementById("display");
 
+const units = {
+  metric: ["C", "km/h"],
+  imperial: ["F", "mph"],
+};
+
+let currentUnit = "metric";
+
 const displayWeather = ({ ...weatherData }) => {
+  const displayUnits = units[currentUnit];
   display.innerHTML = `
   <div>
     <h1>${weatherData.name}</h1>
     <h3>${weatherData.country}</h3>
     <h2>${weatherData.condition.text}</h2>
-    <h2>${weatherData.celcius}</h2>
+    <h2>${weatherData.celcius} ${displayUnits[0]}</h2>
   </div>
   <div>
-
+  <h4>${weatherData.feelsCelcius} ${displayUnits[0]}</h4>
+  <h4>${weatherData.windK} ${displayUnits[1]}</h4>
+  <h4>${weatherData.humidity}</h4>
+  <h4>${weatherData.uv}</h4>
   </div>
   `;
 };
